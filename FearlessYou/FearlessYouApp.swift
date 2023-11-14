9ae57ccf8a15e734b11fe8e7e3c3b6abfee31121
EilafMohammad <elafmohammadcs@gmail.com>
2023-11-14 11:07:36 +0300

    Initial Commit

---
+++ FearlessYou/FearlessYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  FearlessYouApp.swift
+//  FearlessYou
+//
+//  Created by Eilaf Mohammad on 30/04/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct FearlessYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
